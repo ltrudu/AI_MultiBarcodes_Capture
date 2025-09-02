a51e2af4636ee6376474d7f7086606a7e9dd5e86
Trudu Laurent <laurent@trudu.com>
2025-09-02 17:27:50 +0200

    Version Upgrade

--- AI_MutliBarcodes_Capture/build.gradle.kts
+++ AI_MutliBarcodes_Capture/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.zebra.ai_multibarcodes_capture"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
