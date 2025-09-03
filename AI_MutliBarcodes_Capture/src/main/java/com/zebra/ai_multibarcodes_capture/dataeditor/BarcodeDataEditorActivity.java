0e44db842532cf265474f1347ef687e4a9fe72f5
Trudu Laurent <laurent@trudu.com>
2025-09-03 14:19:25 +0200

    WIP: BarcodeEditorActivity

---
+++ AI_MutliBarcodes_Capture/src/main/java/com/zebra/ai_multibarcodes_capture/dataeditor/BarcodeDataEditorActivity.java
@@ -0,0 +1,26 @@
+package com.zebra.ai_multibarcodes_capture.dataeditor;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.zebra.ai_multibarcodes_capture.R;
+
+public class BarcodeDataEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_barcode_data_editor);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
