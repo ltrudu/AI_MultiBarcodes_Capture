94c58c2d762da5d87a34f7ec8b51803f3a7ffe06
Trudu Laurent <laurent@trudu.com>
2025-09-03 13:23:03 +0200

    Add Merge Quantities to session viewer and fixed bugs on BarcodeCapture merge quantities

--- AI_MutliBarcodes_Capture/src/main/java/com/zebra/ai_multibarcodes_capture/helpers/SessionData.java
+++ AI_MutliBarcodes_Capture/src/main/java/com/zebra/ai_multibarcodes_capture/helpers/SessionData.java
@@ -6,14 +6,14 @@ import java.util.Map;
 
 public class SessionData
 {
-    public Map<Integer, String>     barcodeValues;
+    public Map<Integer, String>     barcodeValuesMap;
     public Map<Integer, Integer>    barcodeQuantityMap;
     public Map<Integer, Integer>    barcodeSymbologyMap;
     public Map<Integer, Date>       barcodeDateMap;
 
     public SessionData()
     {
-        barcodeValues = new HashMap<>();
+        barcodeValuesMap = new HashMap<>();
         barcodeQuantityMap = new HashMap<>();
         barcodeSymbologyMap = new HashMap<>();
         barcodeDateMap = new HashMap<>();
