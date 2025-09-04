8856f85a7cdbfd45b15cb4264d9dfbec8445ad97
Trudu Laurent <laurent@trudu.com>
2025-09-04 14:36:02 +0200

    Fixed Sharing functionality

--- AI_MutliBarcodes_Capture/build.gradle.kts
+++ AI_MutliBarcodes_Capture/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.zebra.ai_multibarcodes_capture.dev"
         minSdk = 34
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.14"
+        versionCode = 15
+        versionName = "1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
