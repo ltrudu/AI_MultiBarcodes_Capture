9286923dcd84e55c868e6e89de73c597590cd9d5
Trudu Laurent <laurent@trudu.com>
2025-09-15 10:48:49 +0200

    WIP Settings Broadcast Receiver

---
+++ AI_MultiBarcodes_Capture/src/main/java/com/zebra/ai_multibarcodes_capture/settings/SettingsBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package com.zebra.ai_multibarcodes_capture.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SettingsBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
